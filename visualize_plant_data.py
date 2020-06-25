1b806a1eb8e1652c860f59686fd15cbe763c183a
TheFruitDude <36520284+TheFruitDude@users.noreply.github.com>
2020-06-25 12:19:41 +0200

    Add files via upload

---
+++ visualize_plant_data.py
@@ -0,0 +1,49 @@
+#api3.geo.admin.ch
+
+# headers = Taxon ID	Gruppe	Trivialname	Taxon	Gemeinde(n)	Kanton(e) 	Fundort	H�he	Koordx	Koordy	Radius	R�umliche Zusammenfassung	Anzahl	n Nachweise 	Jahr	Monat	Tag	Fortpflanzungsnachweis	Herkunft	Beobachter/Sammler	ID Beobachtung	Rote Liste	Priorit�t CH	UZL	WZL	Massnahmebedarf	Monitoring-Bedarf	NHV	SMARAGD	infoflora: Kantonaler Schutzstatus	infoflora: Rote Liste regional	Habitat	Substrat	swisslichens: Ecotyp	Ordnung	Familie	swissfungi: funktionelle Gruppe	Indigenat CH	Standortfremd	Einf�hrung
+# Schweizer Koordinaten Bezugsystem = (LV95 CH1903+ / LV95 EPSG 2056)
+import numpy as np
+import pandas as pd
+
+import matplotlib.pyplot as plt
+
+
+large = 22; med = 16; small = 12
+
+
+
+"""
+params = {'axes.titlesize': large,
+          'legend.fontsize': med,
+          'figure.figsize': (16, 10),
+          'axes.labelsize': med,
+          'axes.titlesize': med,
+          'xtick.labelsize': med,
+          'ytick.labelsize': med,
+          'figure.titlesize': large}
+plt.rcParams.update(params)
+
+"""
+
+
+
+plant_data = pd.read_csv("MAR infoflora_200274_Pilatus.csv",sep=";", encoding="iso-8859-1", usecols=['Koordx', 'Koordy', 'Trivialname'])
+df = pd.DataFrame(plant_data)
+x1 = []
+y1 = []
+
+x1 = df['Koordx'].to_list()
+y1 = df['Koordy'].to_list()
+name = df['Trivialname'].to_list()
+
+
+plt.style.use('seaborn-whitegrid')
+plt.xlabel('Koordx')
+plt.ylabel('Koordy')
+plt.title('Scatter Plot')
+
+plt.scatter(np.array(x1), np.array(y1))
+
+plt.show()
+
+
